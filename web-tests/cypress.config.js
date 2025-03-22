7d2de071643adc2cedf80aaafbddcf37521d026e
Ericton Brito <erictonbrito@outlook.com>
2025-03-21 22:31:50 -0400

    feat: implementar cadastro de usuário com validações e feedback visual

---
+++ web-tests/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://fakerapi.it/api/v2/persons?_quantity=1&_gender=female&_birthday_start=2005-01-01',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
