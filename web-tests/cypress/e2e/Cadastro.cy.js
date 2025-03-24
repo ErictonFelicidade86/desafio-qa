5bac2cba6aa5feacbf8ba6aa36a06bdb282fb367
Ericton Brito <erictonbrito@outlook.com>
2025-03-23 20:51:58 -0400

    refactor: code Page Cadastro

--- web-tests/cypress/e2e/Cadastro.cy.js
+++ web-tests/cypress/e2e/Cadastro.cy.js
@@ -24,72 +24,115 @@ const viewports = [
 ]
 
 describe('Cadastro', () => {
-  beforeEach(() => {
+  it('Deve cadastrar um novo usuário', () => {
     cy.visit('/')
     login.go()
     user.accessSignUp()
-  })
-  it('Deve cadastrar um novo usuário', () => {
     user.formUser()
   })
 
   it('Tentar cadastrar um usuário ja existente', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.existingUser()
   })
 
   it('Tentar cadastrar um usuário sem preencher campos obrigatórios', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.requiredFields()
   })
 
   it('Tentar cadastrar um usuário sem informar o nome', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.emptyName()
   })
 
   it('Tentar cadastrar um usuário sem informar o email', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.emptyEmail()
   })
 
   it('Tentar cadastrar um usuário sem informar a senha', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.emptyPassword()
   })
 
   it('Tentar cadastrar um usuário sem informar o confirmar senha', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.emptyConfirmPassword()
   })
 
   it('Tentar cadastrar um usuário com nome passe de 50 caracteres', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.nameInvalid()
   })
 
   it('Tentar cadastrar um usuário com um email inválido', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.emailInvalid()
   })
 
   it('Tentar cadastrar um usuário com senha inválida', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.passwordInvalid()
   })
 
   it('Tentar cadastrar um usuário com senha curta', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.shortPassword()
   })
 
   it('Tentar cadastrar um usuário com senha fora do padrão', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.nonStandardPassword()
   })
 
   it('Tentar cadastrar um usuário com senha sem letra', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.passwordWithoutLetters()
   })
 
   it('Tentar cadastrar um usuário com senha sem número', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.passwordWithoutNumbers()
   })
 
   it('Tentar cadastrar um usuário com uma senha diferente de confirmar senha', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.passwordDifferent()
   })
 
   it('Tentar cadastrar um usuário com confirmar senha diferente de senha', () => {
+    cy.visit('/')
+    login.go()
+    user.accessSignUp()
     user.passwordConfirmDifferent()
   })
 })
@@ -106,68 +149,110 @@ describe('Cadastro Responsivo Page', () => {
         } else {
           cy.viewport(vp)
         }
-        cy.visit('/')
-        login.go()
-        user.accessSignUp()
       })
     
       it('Tentar cadastrar um usuário ja existente', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.existingUser()
       })
     
       it('Tentar cadastrar um usuário sem preencher campos obrigatórios', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.requiredFields()
       })
     
       it('Tentar cadastrar um usuário sem informar o nome', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.emptyName()
       })
     
       it('Tentar cadastrar um usuário sem informar o email', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.emptyEmail()
       })
     
       it('Tentar cadastrar um usuário sem informar a senha', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.emptyPassword()
       })
     
       it('Tentar cadastrar um usuário sem informar o confirmar senha', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.emptyConfirmPassword()
       })
     
       it('Tentar cadastrar um usuário com nome passe de 50 caracteres', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.nameInvalid()
       })
     
       it('Tentar cadastrar um usuário com um email inválido', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.emailInvalid()
       })
     
       it('Tentar cadastrar um usuário com senha inválida', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.passwordInvalid()
       })
     
       it('Tentar cadastrar um usuário com senha curta', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.shortPassword()
       })
     
       it('Tentar cadastrar um usuário com senha fora do padrão', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.nonStandardPassword()
       })
     
       it('Tentar cadastrar um usuário com senha sem letra', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.passwordWithoutLetters()
       })
     
       it('Tentar cadastrar um usuário com senha sem número', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.passwordWithoutNumbers()
       })
     
       it('Tentar cadastrar um usuário com uma senha diferente de confirmar senha', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.passwordDifferent()
       })
     
       it('Tentar cadastrar um usuário com confirmar senha diferente de senha', () => {
+        cy.visit('/')
+        login.go()
+        user.accessSignUp()
         user.passwordConfirmDifferent()
       })
     })
