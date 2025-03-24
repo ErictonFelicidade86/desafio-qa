30ad9897f715385dd5238e2f70a170604c6d234c
Ericton Brito <erictonbrito@outlook.com>
2025-03-24 01:05:30 -0400

    feat: integrate Allure Report for test result visualization

--- web-tests/cypress.config.js
+++ web-tests/cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3010',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureCypress(on, config);
+      return config;
     },
-    screenshotOnRunFailure: false
+    screenshotOnRunFailure: true,
+    video: true
   },
 });
